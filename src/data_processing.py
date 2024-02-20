49224185761a18b7ce4a41f9f1046ef492e40c52
Diego-Alvarado <diego.alvarado-maldonado@strath.ac.uk>
2024-02-20 00:50:19 +0000

    fix dir

--- src/data_processing.py
+++ src/data_processing.py
@@ -2,10 +2,10 @@ import random
 import pandas as pd
 
 def load_df(file_names: dict, key: int) -> tuple[pd.DataFrame]:
-    df_materials = pd.read_csv(f'./src/data/sample_cvae/{file_names[key]["materials"]}', sep='\t')
+    df_materials = pd.read_csv(f'data/sample_cvae/{file_names[key]["materials"]}', sep='\t')
     df_materials['structure'] = [file_names[key]["materials"][:-4] + f'_{id}.png' for id in df_materials['index']]
     
-    df_sequences = pd.read_csv(f'./src/data/sample_cvae/{file_names[key]["sequences"]}', sep='\t')
+    df_sequences = pd.read_csv(f'data/sample_cvae/{file_names[key]["sequences"]}', sep='\t')
     df_sequences = df_sequences.fillna('')
     cols = ['A', 'B']
     random.seed(key)
